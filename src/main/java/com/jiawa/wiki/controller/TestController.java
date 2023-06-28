c77ba756e8516293055861aa88fe149cea333543
locvye <2427112539@qq.com>
2023-06-28 13:58:04 +0800

    2-7 增加一个hello测试接口

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -3,6 +3,7 @@ package com.jiawa.wiki.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+
 @RestController  //一般返回字符串  @ResponseBody 用来返回字符串或JSON对象
 //@Controller // 返回页面
 public class TestController {
@@ -15,8 +16,12 @@ public class TestController {
 //    @PutMapping()
 //    @DeleteMapping()
 //    @RequestMapping(value="",method = RequestMethod) 定义方法类型
-    @RequestMapping("/hello") //变成一个接口
+    @GetMapping("/hello") //变成一个接口
     public String hello(){
-        return "Hello Word！";
+        return "Hello World";
+    }
+    @PostMapping("/hello/post") //变成一个接口
+    public String helloPost(String name){
+        return "Hello Word! post," + name;
     }
 }
